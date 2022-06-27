86458d76d59b2045df911186379bcf265ded4354
Ant <antvilladean@googlemail.com>
2022-06-27 18:48:38 +0200

    Added Hamburger Menu

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { connect } from "./redux/blockchain/blockchainActions";
 import { fetchData } from "./redux/data/dataActions";
 import * as s from "./styles/globalStyles";
 import styled from "styled-components";
+import Menu from "./Menu/Menu";
 
 const truncate = (input, len) =>
   input.length > len ? `${input.substring(0, len)}...` : input;
@@ -223,6 +224,7 @@ function App() {
 
   return (
     <s.Screen>
+      <Menu/>
       <s.Container
         flex={1}
         ai={"center"}
---
+++ src/Menu/Menu.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { slide as Menu } from 'react-burger-menu';
+import './Menu.css';
+
+export default props => {
+  return (
+    <Menu>
+      <a className="menu-item" href="/">
+        Home
+      </a>
+      <a className="menu-item" target="_blank" href="https://not-another-ghost-collection.gitbook.io/nagc-documentation/">
+        Documentation
+      </a>
+    </Menu>
+  );
+};
