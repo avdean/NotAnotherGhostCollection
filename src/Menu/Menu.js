4d921e40fe467800c58ff375abe02cf337226b0d
Ant <antvilladean@googlemail.com>
2022-06-28 09:32:05 +0200

    Added instagram

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -8,6 +8,9 @@ export default props => {
       <a className="menu-item" href="/">
         Home
       </a>
+      <a className="menu-item" target="_blank" href="https://instagram.com/not.another.ghost.collection">
+        Instagram
+      </a>
       <a className="menu-item" target="_blank" href="https://not-another-ghost-collection.gitbook.io/nagc-documentation/">
         Documentation
       </a>
