1421763b4972aaac487443b1bf8f8aab1b52395d
Avdean <34350184+avdean@users.noreply.github.com>
2022-07-06 18:58:03 +0200

    Took off to zeroes

--- src/App.js
+++ src/App.js
@@ -294,7 +294,7 @@ function App() {
                 >
                 <p>Not Another Ghost Collection features dynamic pricing.<br></br></p>
                 <p>You can find out the exact specifics of NAGC's minting prices in the <a href="https://not-another-ghost-collection.gitbook.io/nagc-documentation/pricing-and-use-cases" target="_blank">Documentation</a>.<br></br></p>
-                <p>Currently price of individual mint is {data.cost / 10000000000000000000} FTM.</p>
+                <p>Currently price of individual mint is {data.cost / 100000000000000000} FTM.</p>
                 </s.TextTitle>
                 <s.SpacerXSmall />
                 <s.TextDescription
